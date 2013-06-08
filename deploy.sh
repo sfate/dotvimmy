d9c2feb6820c4d75ed050b76e38c7be0a67da24c
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2013-06-08 21:48:12 +0300

    No hidden files in repo :recycle:

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 cd $HOME && git clone git://github.com/Sfate/Vim-environment.git Vim-environment
 [ -d .vim ] && mv .vim{,.old}
-mv Vim-environment/.vim* .
+mv Vim-environment/vim .vim
+mv Vim-environment/vimrc .vimrc
 mkdir -p .vim/bundle
 git clone git://github.com/gmarik/vundle.git .vim/bundle/vundle
 rm -rf Vim-environment .vim.old
