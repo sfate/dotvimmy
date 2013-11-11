a380260b7ca2210816e9fbd1a6946e56ed5fbb7e
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2013-11-11 20:41:58 +0200

    Use non-auth way at cloning vim-vundle + added time_spent counter.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
 set -e
 cd $HOME
 
+start_time=$(date +%s)
+echo "[*] Starting deploy of vim configuration."
+
 (wget -N -O .vimrc https://raw.github.com/Sfate/Vim-environment/master/vimrc) > /dev/null 2>&1
 [ -d .vim ] && rm -rf .vim
 mkdir -p .vim/bundle
-(git clone git://github.com/gmarik/vundle.git .vim/bundle/vundle) > /dev/null 2>&1
+(git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle) > /dev/null 2>&1
 
 vim +BundleInstall +qall < /dev/tty > /dev/tty
+
+end_time=$(date +%s)
+echo "[*] Deployed successfully. Time spent: $((end_time-start_time))s."
 
 exit 0
